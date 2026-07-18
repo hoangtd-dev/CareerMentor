41f45ad6a111769fe6533a1f15289786dbc8fde6
duy hoang <hoangtd.dev@gmail.com>
2026-07-18 22:17:57 +1000

    implement tree

---
+++ DataStructure/Tree/OriginalTree/BETreeNode.java
@@ -0,0 +1,4 @@
+public class BETreeNode {
+	public int value;
+	public ArrayList<BETreeNode> children;
+}
---
+++ DataStructure/Tree/OriginalTree/Tree.java
@@ -0,0 +1,39 @@
+import java.util.LinkedList;
+import java.util.Queue;
+
+import javax.swing.tree.BETreeNode;
+
+public class Tree {
+	public BETreeNode root;
+
+	public BETreeNode BFSearch(int value) {
+		Queue<BETreeNode> nodes = new LinkedList<>();
+		nodes.add(root);
+
+		while (!nodes.isEmpty()) {
+			BETreeNode currentNode = nodes.poll();
+
+			if (currentNode.value)
+				return currentNode;
+
+			for (BETreeNode item : nodes.children) {
+				nodes.add(item);
+			}
+		}
+
+		return null;
+	}
+
+	public BETreeNode DFSearch(BETreeNode node, int value) {
+		if (value == node.value)
+			return node;
+
+		for (BETreeNode item : node.children) {
+			BETreeNode result = DFSearch(item, value);
+			if (result != null)
+				return result;
+		}
+
+		return null;
+	}
+}
