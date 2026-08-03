79df426361b8a76b6fe2e66223f49b9db1da333d
duy hoang <hoangtd.dev@gmail.com>
2026-08-03 23:55:47 +1000

    two sum

---
+++ leetcode/TwoSum/Main.java
@@ -0,0 +1,11 @@
+public class Main {
+    // requirement: https://leetcode.com/problems/two-sum/
+    public static void main(String[] args) {
+
+    }
+
+    public int[] twoSum(int[] nums, int target) {
+        // var map = new HashMap<Integer, Integer>();
+        // for () {}
+    }
+}
